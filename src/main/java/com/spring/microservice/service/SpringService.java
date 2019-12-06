a547ca91e62e80b092657b8245a5b6245ce9f0d5
Augusto Teodoro <teodoroaraujo@gmail.com>
2019-12-05 22:43:51 -0300

    Create method of call service

--- src/main/java/com/spring/microservice/service/SpringService.java
+++ src/main/java/com/spring/microservice/service/SpringService.java
@@ -1,6 +1,14 @@
 package com.spring.microservice.service;
 
+import java.util.concurrent.CompletableFuture;
+
+import com.spring.microservice.response.UserResponse;
+
 public interface SpringService
 {
 
+    CompletableFuture<UserResponse> getUser();
+
+
+    CompletableFuture<UserResponse> getCommentByUser(UserResponse user);
 }
--- src/main/java/com/spring/microservice/service/impl/SpringServiceImpl.java
+++ src/main/java/com/spring/microservice/service/impl/SpringServiceImpl.java
@@ -1,8 +1,11 @@
 package com.spring.microservice.service.impl;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.spring.microservice.response.UserResponse;
 import com.spring.microservice.service.SpringService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,4 +20,18 @@ public class SpringServiceImpl implements SpringService
     private static final String USER_URL = "http://jsonplaceholder.typicode.com/users/1";
     private static final String COMMENT_URL = "http://jsonplaceholder.typicode.com/posts?userId=1";
     private RestTemplate restTemplate = new RestTemplate();
+    
+    
+    @Override
+    public CompletableFuture<UserResponse> getUser()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public CompletableFuture<UserResponse> getCommentByUser(UserResponse user)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
